6f94e6568949a6af5fc2e441fd7de5651c26c1d6
offhanddefense <offhanddefense@users.noreply.github.com>
2017-05-31 07:57:46 -0700

    New scripts
    Redacted scripts

---
+++ DailyHealthScript.ps1
@@ -0,0 +1,36 @@
+﻿# #############################################################################
+# CompanyD - SCRIPT - POWERSHELL
+# NAME: DailyHealthScript.ps1
+# 
+# AUTHOR:  OffHand
+# DATE:  2017.03.21
+# EMAIL: OffHand
+# 
+# COMMENT:  This script will run across several critical systems to query 
+#           their health status and send a report to technical SysAdmins.
+#
+# VERSION HISTORY
+# 1.0 2017.03.21 Initial Version.
+# 
+#
+# TO ADD
+# -Add a Function to email
+# -Fix the...
+# #############################################################################
+
+
+
+$reportpath = "\\fs1\departments\Information Systems\Infrastructure\Systems\"
+$date = get-date -Format yyyyMMdd
+$time = get-date -Format HHmm
+
+# Exchange
+#get-serverhealth -id dmgiovexch03 | ? {$_.alertvalue -ne "Healthy" -and $_.alertvalue -ne "Disabled" }
+
+# Active Directory
+dcdiag /e /v /c /s:IODC1 /f:"$reportpath\active directory\$date dcdiag.txt"
+repadmin /queue * > "$reportpath\active directory\$date repadmin.txt"
+repadmin /replsummary >> "$reportpath\active directory\$date repadmin.txt"
+
+# Group Policy
+#Get-GPOReport -All -Path "$reportpath\active directory\$date FullGPOReport.html" -ReportType Html  -Server IODC1 
---
+++ WeeklyHealthScript.ps1
@@ -0,0 +1,29 @@
+﻿# #############################################################################
+# CompanyD - SCRIPT - POWERSHELL
+# NAME: WeeklyHealthScript.ps1
+# 
+# AUTHOR:  OffHand
+# DATE:  2017/05/31
+# EMAIL: Offhand
+# 
+# COMMENT:  This script will perform health checks and tasks on a weekly basis
+#           to maintain CompanyD's environment
+#
+# VERSION HISTORY
+# 1.0 20170531 Initial Version.
+# 1.1 
+#
+# TO ADD
+# -Add a Function to ...
+# -Fix the...
+# #############################################################################
+
+$systemspath = "\\fs1\departments\Information Systems\Infrastructure\Systems\"
+$date = get-date -Format yyyyMMdd
+
+#Backup Group Policies
+$gpodescription = "Weekly backup of Group Policies for comparision or small restores"
+$gpopath = $systemspath + "Active Directory\GroupPolicy\Group Policy Backup $date\"
+New-Item $gpopath -ItemType Directory
+Backup-GPO -All -Comment $gpodescription -Path $gpopath -Server iodc1 
+
